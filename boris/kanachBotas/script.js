f571c41349cd8fcbe9d97da154116168734a6b53
Boris Jalalyan <borisjalalyan10@gmail.com>
2021-03-23 01:01:17 +0400

    verjnakan tesqi berum bacarutyunnerov

--- boris/kanachBotas/script.js
+++ boris/kanachBotas/script.js
@@ -1,66 +1,87 @@
 let element = document.getElementsByClassName("img")[0];
 let element2 = document.getElementsByClassName("info")[0];
 let element3 = document.getElementsByClassName("klor")[0];
-const elem = document.getElementById("image")
-let lists = [document.getElementById("li1"), document.getElementById("li2"), document.getElementById("li3"), document.getElementById("li4")]
+const elem = document.getElementById("image");
+let lists = [
+    document.getElementById("li1"),
+    document.getElementById("li2"),
+    document.getElementById("li3"),
+    document.getElementById("li4"),
+];
 element.classList.add("animacia1");
 element2.classList.add("hiddenBlock");
 element3.classList.add("poqrKlor");
+const wait = (timeout => new Promise(resolve => setTimeout(resolve, timeout)));
 
 elem.addEventListener("mouseover", () => {
+
     element.classList.remove("animacia1");
     element.classList.add("animacia");
     element2.classList.remove("hiddenBlock");
     element2.classList.add("blochNoHidden");
     element3.classList.remove("poqrKlor");
     element3.classList.add("metsKlor");
+    console.log("waiting");
 });
-
 elem.addEventListener("mouseleave", () => {
+
     element.classList.remove("animacia");
     element.classList.add("animacia1");
     element2.classList.remove("blochNoHidden");
     element2.classList.add("hiddenBlock");
     element3.classList.remove("metsKlor");
     element3.classList.add("poqrKlor");
-});
 
-let isActivate = true;
-lists.forEach(element => {
-    element.addEventListener("click", () => {
-        isActivate = !isActivate;
-        element.style.backgroundColor = element.style.backgroundColor == "#A0D727" ? "#fcfbfc" : "A0D727";
-        if (element.style.backgroundColor == "#A0D727") {
-            element.style.backgroundColor = "#fcfbfc";
-        }
+});
+let isActivate = false;
+console.log(lists);
+lists.forEach((elements) => {
+    elements.style.backgroundColor = "#fcfbfc";
+    elements.addEventListener("click", () => {
+        elements.style.backgroundColor = elements.style.backgroundColor == "#A0D727" ? "#fcfbfc" : "#A0D727";
+        isActivate = true;
         if (isActivate) {
-            lists.forEach(naxordElement => {
-                naxordElement.style.backgroundColor = "fcfbfc";
-                if (naxordElement == element) {
-                    // chi ashxatum
+            isActivate = false;
+            lists.forEach((naxordElements) => {
+                if (naxordElements == elements) {
+                    console.log(naxordElements, elements);
                     console.log("sas");
                     return 0;
                 }
+                naxordElements.style.backgroundColor = "#fcfbfc";
             });
         }
         console.log(isActivate, !isActivate);
     });
 });
 
-let shoosColor = document.getElementsByClassName("guyner");
-shoosColor.forEach(element => {
-    element.addEventListener("click", () => {
-        element.style.backgroundColor = element.style.backgroundColor == "#A0D727" ? "A0D727" : "A0D727";
-        if (element.style.backgroundColor == "#A0D727") {
-            element.style.backgroundColor
+let shoosColor = Array.prototype.slice.call(document.getElementsByClassName("guyner"));
+console.log(shoosColor[0]);
+console.log(shoosColor[1]);
+console.log(shoosColor[2]);
+shoosColor.forEach((elements) => {
+    elements.style.borderColor = "#fcfbfc";
+    elements.addEventListener("click", () => {
+        elements.style.borderColor = elements.style.borderColor == "#A0D727" ? "#fcfbfc" : "#A0D727";
+        isActivate = true;
+        if (isActivate) {
+            isActivate = false;
+            shoosColor.forEach((naxordElements) => {
+                if (naxordElements == elements) {
+                    console.log(naxordElements, elements);
+                    console.log("sas");
+                    return 0;
+                }
+                naxordElements.style.borderColor = "#fcfbfc";
+            });
         }
-        isActivate = !isActivate;
         console.log(isActivate, !isActivate);
     });
 });
-
 let x = 50;
+
+
 console.log((x * 100) / window.innerWidth);
 console.log((x * 100) / window.innerHeight);
 
-console.log(a);
+// console.log(a);
